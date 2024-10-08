168a307d7ac0761cff8e905f144256667296e364
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-08 15:35:42 +0200

    add bonus

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,21 +3,22 @@ const btnEl =document.getElementById('btn')
 console.log(btnEl);
 console.log(bulbEl);
 
-if (btnEl.innerHTML.includes('Accendi')) {
-    btnEl.addEventListener('click',() =>{
-    
-        bulbEl.src = "./assets/img/yellow_lamp.png"
-        btnEl.innerHTML ='Spegni'
-    })
-    
-}else{
 
     btnEl.addEventListener('click',() =>{
-    
-        bulbEl.src = "./assets/img/white_lamp.png"
-        btnEl.innerHTML ='Accendi'
+        if (btnEl.innerHTML.includes('Accendi')) {
+        bulbEl.src = "./assets/img/yellow_lamp.png"
+        btnEl.innerHTML ='Spegni'     
+        }else{
+          bulbEl.src = "./assets/img/white_lamp.png"
+        btnEl.innerHTML ='Accendi'  
+        }
+       
     })
-}
+    
+
+
+
+
 
 
 

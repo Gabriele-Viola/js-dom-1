421adcca44f1fc5f67b5172535614c356d9ed8e5
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-08 15:32:29 +0200

    try to shout down bulb

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,11 +3,21 @@ const btnEl =document.getElementById('btn')
 console.log(btnEl);
 console.log(bulbEl);
 
+if (btnEl.innerHTML.includes('Accendi')) {
+    btnEl.addEventListener('click',() =>{
+    
+        bulbEl.src = "./assets/img/yellow_lamp.png"
+        btnEl.innerHTML ='Spegni'
+    })
+    
+}else{
 
-btnEl.addEventListener('click',() =>{
-    bulbEl.src = "./assets/img/yellow_lamp.png"
-    btnEl.innerHTML ='Spegni'
-})
+    btnEl.addEventListener('click',() =>{
+    
+        bulbEl.src = "./assets/img/white_lamp.png"
+        btnEl.innerHTML ='Accendi'
+    })
+}
 
 
 

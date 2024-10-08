bb05e6921a284e4f35c9c089f6ca597936b501b4
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-08 16:16:53 +0200

    add css Style

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,21 +1,33 @@
 const bulbEl = document.getElementById('bulb')
-const btnEl =document.getElementById('btn')
+const btnEl = document.getElementById('btn')
+const bodyEl = document.getElementById('body')
+const pEl = document.getElementById('paragh')
 console.log(btnEl);
 console.log(bulbEl);
 
 
-    btnEl.addEventListener('click',() =>{
-        if (btnEl.innerHTML.includes('Accendi')) {
+btnEl.addEventListener('click', () => {
+    if (btnEl.innerHTML.includes('Accendi')) {
         bulbEl.src = "./assets/img/yellow_lamp.png"
-        btnEl.innerHTML ='Spegni'     
-        }else{
-          bulbEl.src = "./assets/img/white_lamp.png"
-        btnEl.innerHTML ='Accendi'  
-        }
-       
-    })
-    
+        btnEl.innerHTML = 'Spegni'
+        btnEl.classList.add('btnoff')
+        btnEl.classList.remove('btn')
+        bodyEl.classList.remove('off')
+        pEl.innerHTML = 'Ora si vede'
+        pEl.classList.remove('on')
 
+    } else {
+        bulbEl.src = "./assets/img/white_lamp.png"
+        btnEl.innerHTML = 'Accendi'
+        btnEl.classList.add('btn')
+        btnEl.classList.remove('btnoff')
+        bodyEl.classList.add('off')
+        pEl.classList.add('on')
+        pEl.innerHTML = 'Non si vede nulla!'
+
+    }
+
+})
 
 
 

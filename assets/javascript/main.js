273cd5741bd739a1c9c4fed6c72cd11b6166395d
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-08 15:05:52 +0200

    add first change

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const bulbEl = document.getElementById('bulb')
+const btnEl =document.getElementById('btn')
+console.log(btnEl);
+console.log(bulbEl);
+
+
+btnEl.addEventListener('click',() =>{
+    bulbEl.src = "./assets/img/yellow_lamp.png"
+})
+
+
+
